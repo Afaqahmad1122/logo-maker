901271d8db469acf3da8f42f8fd6c7fa8321808f
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2024-05-10 21:57:43 -0700

    added  color picker feature in bgController component

--- src/components/BackgoundController.jsx
+++ src/components/BackgoundController.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Slider } from "./ui/slider";
+import ColorPickerController from "./ColorPickerController";
 
 const BackgoundController = () => {
-  return <div>BackgoundController</div>;
+  const [rounded, setRounded] = useState(0);
+  const [padding, setPadding] = useState(0);
+  const [color, setColor] = useState("#000");
+
+  const storagedValue = JSON.parse(localStorage.getItem("value"));
+
+  useEffect(() => {
+    const updatedValue = {
+      ...storagedValue,
+      bgRounded: rounded,
+      bgPadding: padding,
+      bgColor: color,
+    };
+
+    localStorage.setItem("value", JSON.stringify(updatedValue));
+  });
+
+  return (
+    <div>
+      <div className="py-2">
+        <label className="p-2 flex justify-between items-center">
+          Rounded <span>{rounded} px</span>
+        </label>
+        <Slider
+          defaultValue={[0]}
+          max={512}
+          step={1}
+          onValueChange={(event) => setRounded(event[0])}
+        />
+      </div>
+
+      <div className="py-2">
+        <label className="p-2 flex justify-between items-center">
+          Padding <span>{padding} px</span>
+        </label>
+        <Slider
+          defaultValue={[0]}
+          max={512}
+          step={1}
+          onValueChange={(event) => setPadding(event[0])}
+        />
+      </div>
+
+      <div className="py-2">
+        <label className="p-2 flex justify-between items-center">
+          Color Picker
+        </label>
+
+        <ColorPickerController
+          hideController={false}
+          selectedColor={(color) => setColor(color)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default BackgoundController;
